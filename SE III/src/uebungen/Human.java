4574df6cae42e53a21a43fa34470fb329e0c2ee4
barbara986 <92519356+barbara986@users.noreply.github.com>
2023-01-09 18:33:54 +0100

    Neue Übungen

---
+++ SE III/src/uebungen/Human.java
@@ -0,0 +1,24 @@
+package uebungen;
+
+public class Human {
+    private String name;
+    private String age;
+    // Ganz schön viel Info in einer Klasse. Man kann doch die Adressdaten doch sicher auch in einer eigenen 
+    // Klasse bündeln und auch die Methode dahin machen, wo sie am besten hinpasst oder?
+    private String country;
+    private String city;
+    private String street;
+    private String house;
+
+    public String getFullAddress() {
+        StringBuilder result = new StringBuilder();
+        return result
+                        .append(country)
+                        .append(", ")
+                        .append(city)
+                        .append(", ")
+                        .append(street)
+                        .append(", ")
+                        .append(house).toString();
+    }
+ }
